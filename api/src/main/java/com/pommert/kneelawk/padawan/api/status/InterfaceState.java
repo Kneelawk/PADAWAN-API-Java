f9d4d3d337ac2a67c56d164ebceb89b4af5ae9e6
Kneelawk <jedidiah.pommert@gmail.com>
2017-06-29 11:54:47 -0700

    Begin to flesh out status stuff

---
+++ api/src/main/java/com/pommert/kneelawk/padawan/api/status/InterfaceState.java
@@ -0,0 +1,5 @@
+package com.pommert.kneelawk.padawan.api.status;
+
+public enum InterfaceState {
+	NOT_INITIALIZED, INITIALIZED, RUNNING, STOPPED, CLOSED, ERROR
+}
--- api/src/main/java/com/pommert/kneelawk/padawan/api/status/InterfaceStatus.java
+++ api/src/main/java/com/pommert/kneelawk/padawan/api/status/InterfaceStatus.java
@@ -1,6 +1,21 @@
 package com.pommert.kneelawk.padawan.api.status;
 
-// TODO what would the status hold?
-public class InterfaceStatus {
+import java.util.Optional;
 
+public class InterfaceStatus {
+	private InterfaceState state;
+	private Optional<Throwable> error;
+
+	public InterfaceStatus(InterfaceState state, Optional<Throwable> error) {
+		this.state = state;
+		this.error = error;
+	}
+
+	public InterfaceState getState() {
+		return state;
+	}
+
+	public Optional<Throwable> getError() {
+		return error;
+	}
 }
